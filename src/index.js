ea07fb2a895d0b35352a0b86936170eb19eaeb9a
Sergio Lucena <sergio.lucena@raisin.com>
2020-02-17 23:43:43 +0100

    WIP:Feat(redux): Configures redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, compose } from "redux";
+import reducers from "./store/reducers";
 import App from "./App.js";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
---
+++ src/store/auth/reducers/index.js
@@ -0,0 +1,13 @@
+import { SIGN_IN, SIGN_OUT } from "../actions/types";
+import { INITIAL_STATE } from "../selectors";
+
+export const authReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      return { ...state, isSignedIn: true, userId: action.payload };
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null };
+    default:
+      return state;
+  }
+};
