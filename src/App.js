331b036ca0bd46c6b8d897421c067d60c8888dbc
Sergio Lucena <sergio.lucena@raisin.com>
2020-03-17 23:35:20 +0100

    WIP:Feat(Button): Adds the Button component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
 import Header from "./components/Header";
+import Button from "./components/Button";
 
 const App = () => {
   return (
     <div className="wrapper">
       <GlobalStyle />
       <Header />
+      <Button>Login</Button>
     </div>
   );
 };
---
+++ src/components/Button/index.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  display: block;
+  border: none;
+  border-radius: 5px;
+  color: #FF7F00;
+  background-color: #ffffff;
+  text-align: center;
+  padding: 10px 20px;
+  font-size: 1.75em;
+  text-transform: uppercase;
+  font-weight: bold;
+  width: 100%;
+  max-width: 330px;
+`;
+
+export default Button;
